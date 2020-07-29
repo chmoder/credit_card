10bb9761ec945d3a0d7c6299ae97cc6bc21fc831
tcross <tom.bz2@gmail.com>
2020-07-28 20:24:11 -0500

    initial commit

---
+++ src/credit_card.rs
@@ -0,0 +1,35 @@
+use serde::{Serialize, Deserialize};
+use crate::validate::Validate;
+
+#[derive(Serialize, Deserialize, Clone, Debug, Default)]
+pub struct CreditCard {
+    pub number: String,
+    pub cardholder_name: String,
+    pub expiration_month: String,
+    pub expiration_year: String,
+    pub brand: Option<String>,
+    pub security_code: Option<String>
+}
+
+impl CreditCard {
+    pub fn new() -> Self {
+        CreditCard {
+            number: "".to_string(),
+            cardholder_name: "".to_string(),
+            expiration_month: "".to_string(),
+            expiration_year: "".to_string(),
+            brand: None,
+            security_code: None
+        }
+    }
+
+    pub fn apply_brand(&mut self) {
+        match Validate::from(self.number.clone().as_str()) {
+            Ok(result) => {
+                println!("Card type is: {}", result.card_type.name());
+                self.brand = Option::from(result.card_type.name());
+            },
+            Err(err) => println!("Card is invalid: {:?}", err)
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+//! Credit Card is a library for adding credit cards
+//! to any project.
+//!
+//! ```rust,no_run
+//! use credit_card::CreditCard;
+//!
+//! let mut cc = CreditCard {
+//!     number: "4111111111111111".to_string(),
+//!     cardholder_name: "Graydon Hoare".to_string(),
+//!     expiration_month: "01".to_string(),
+//!     expiration_year: "2023".to_string(),
+//!     brand: None,
+//!     security_code: None
+//! };
+//! cc.apply_brand();
+//! ```
+//!
+//! # Current Features
+//! - Create CreditCards
+//! - Add brand to credit cards from card number
+//! - Validate credit card number
+//!
+//! # Future Features
+//! - Validate CVV
+//! - Validate Address
+//! - ...
+//! - Full card validation
+//!
+//! ### Notice:
+//! This is under development right now, so interfaces
+//! and apis will be changing.  If you are interested
+//! in using this please create an issue or reach out
+//! with your feature request so I can help add it.
+//!
+mod luhn;
+mod validate;
+mod credit_card;
+
+
+// lazy static is used by `validate`
+// `macro_use` is required to be at the module root
+#[macro_use]
+extern crate lazy_static;
+
+// re-export CreditCard
+pub use credit_card::CreditCard;
+
+#[cfg(test)]
+mod tests {
+    use crate::CreditCard;
+
+    #[test]
+    fn create() {
+        let cc = CreditCard {
+            number: "4111111111111111".to_string(),
+            cardholder_name: "Graydon Hoare".to_string(),
+            expiration_month: "01".to_string(),
+            expiration_year: "2023".to_string(),
+            brand: None,
+            security_code: None
+        };
+        assert_eq!(cc.number, "4111111111111111".to_string())
+    }
+
+    #[test]
+    fn is_visa() {
+        let mut cc = CreditCard {
+            number: "4111111111111111".to_string(),
+            cardholder_name: "Graydon Hoare".to_string(),
+            expiration_month: "01".to_string(),
+            expiration_year: "2023".to_string(),
+            brand: None,
+            security_code: None
+        };
+        cc.apply_brand();
+
+        assert_eq!(cc.brand.unwrap_or_default(), "visa")
+    }
+
+}
